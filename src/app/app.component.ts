785a00b17e66b3cbeeb96926198fad945caed64a
Adrian Tomasek <extern.adrian.tomasek@esolutions.de>
2022-01-25 16:20:35 +0100

    add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedNav = 'recipe';
+
+  onNavClicked(target: string) {
+    this.loadedNav = target;
+  }
 }
