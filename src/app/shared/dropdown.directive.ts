98659fc72a641def01061d259ffbafb3a7a69233
Adrian Tomasek <extern.adrian.tomasek@esolutions.de>
2022-01-26 08:45:20 +0100

    some enhancement on dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,12 +1,13 @@
-import {Directive, HostBinding, HostListener} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener} from "@angular/core";
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
-
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // Dadurch wird dropdown auch geschlossen, wenn man irgendwo außerhalb klickt
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
