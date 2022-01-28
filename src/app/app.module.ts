7e8e24af6c93895e2cb8ed9b2c1406b864695639
Adrian Tomasek <extern.adrian.tomasek@esolutions.de>
2022-01-28 15:29:45 +0100

    added shopping-list animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
 import {SharedModule} from "./shared/shared.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {SharedModule} from "./shared/shared.module";
     SharedModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,11 +2,49 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "./shopping-list.service";
 import {Subscription} from "rxjs";
+import {animate, group, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      state('appear', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      // Animation von "nicht da" nach "irgendwas" (Beim Einfügen eines Listenelements)
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)',
+          color: 'green'
+        }),
+        group([
+          animate(300, style({
+            opacity: 1,
+            transform: 'translate(0px)'
+          })),
+          animate(800, style({
+            color: '#555'
+          }))
+        ])
+      ]),
+      // Animation von "irgendwas" nach "nichts" (Beim Löschen eines Listenelements)
+      transition('* => void', [
+        group([
+          animate(800, style({
+          opacity: 0,
+          transform: 'translate(100px)'
+        })),
+          animate(300, style({
+            color: 'red'
+          }))
+        ])
+      ])
+    ])
+  ]
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
@@ -24,6 +62,14 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   onEditItem(index: number) {
     this.shoppingListService.startedEditing.next(index);
+  }
+
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event);
   }
 
   ngOnDestroy() {
